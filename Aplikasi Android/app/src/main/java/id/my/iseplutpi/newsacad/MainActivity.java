a87eae29b6db9b7a7beafe179cb222734039df18
Isep Lutpi Nur <iseplutpinur7@gmail.com>
2021-12-30 09:20:59 +0700

    update search

--- Aplikasi Android/app/src/main/java/id/my/iseplutpi/newsacad/MainActivity.java
+++ Aplikasi Android/app/src/main/java/id/my/iseplutpi/newsacad/MainActivity.java
@@ -1,22 +1,31 @@
 package id.my.iseplutpi.newsacad;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import java.util.List;
 
 import id.my.iseplutpi.newsacad.model.NewsApiResponse;
 import id.my.iseplutpi.newsacad.model.NewsHeadlines;
 
-public class MainActivity extends AppCompatActivity implements SelectListener {
+public class MainActivity extends AppCompatActivity implements SelectListener, View.OnClickListener {
     RecyclerView recyclerView;
     CustomAdapter adapter;
     ProgressDialog dialog;
+
+    Button btn_1, btn_2, btn_3, btn_4, btn_5, btn_6, btn_7;
+    private static String lastCategory = "technology";
+
+    SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,21 +36,57 @@ public class MainActivity extends AppCompatActivity implements SelectListener {
         dialog.setTitle("Fetching News Article...");
         dialog.show();
 
+        btn_1 = findViewById(R.id.btn_1);
+        btn_1.setOnClickListener(this);
+        btn_2 = findViewById(R.id.btn_2);
+        btn_2.setOnClickListener(this);
+        btn_3 = findViewById(R.id.btn_3);
+        btn_3.setOnClickListener(this);
+        btn_4 = findViewById(R.id.btn_4);
+        btn_4.setOnClickListener(this);
+        btn_5 = findViewById(R.id.btn_5);
+        btn_5.setOnClickListener(this);
+        btn_6 = findViewById(R.id.btn_6);
+        btn_6.setOnClickListener(this);
+        btn_7 = findViewById(R.id.btn_7);
+        btn_7.setOnClickListener(this);
+
+        searchView = findViewById(R.id.search_view);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                dialog.setTitle("Fetching news article of " + query);
+                dialog.show();
+                RequestManager manager = new RequestManager(MainActivity.this);
+                manager.getNewsHeadlines(listener, "", query);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
 
         RequestManager manager = new RequestManager(this);
-        manager.getNewsHeadlines(listener, "technology", null);
+        manager.getNewsHeadlines(listener, getLastCategory(), null);
     }
 
     private final OnFetchDataListener<NewsApiResponse> listener = new OnFetchDataListener<NewsApiResponse>() {
         @Override
         public void onFetchData(List<NewsHeadlines> list, String message) {
-            showNews(list);
+            if (list.isEmpty()) {
+                Toast.makeText(MainActivity.this, "No Data Found !!", Toast.LENGTH_SHORT).show();
+            } else {
+                showNews(list);
+            }
             dialog.dismiss();
         }
 
         @Override
         public void onError(String message) {
-
+            Toast.makeText(MainActivity.this, "An Error Occured !!", Toast.LENGTH_SHORT).show();
         }
     };
 
@@ -56,6 +101,25 @@ public class MainActivity extends AppCompatActivity implements SelectListener {
     @Override
     public void OnNewsClick(NewsHeadlines headlines) {
         startActivity(new Intent(MainActivity.this, DetailsActivity.class)
-        .putExtra("data", headlines));
+                .putExtra("data", headlines));
+    }
+
+    @Override
+    public void onClick(View v) {
+        Button button = (Button) v;
+        String category = button.getText().toString();
+        dialog.setTitle("Fetching news articles of " + category);
+        dialog.show();
+        RequestManager manager = new RequestManager(this);
+        manager.getNewsHeadlines(listener, category, null);
+        setLastCategory(category);
+    }
+
+    public static String getLastCategory() {
+        return lastCategory;
+    }
+
+    public static void setLastCategory(String lastCategory) {
+        MainActivity.lastCategory = lastCategory;
     }
 }
